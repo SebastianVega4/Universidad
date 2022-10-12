6c62c3441bf8b1c47adaa3153ff658e4e2c53c5c
SebastianVega4 <sebastian.vegar2015@gmail.com>
2022-10-12 09:09:15 -0500

    herencia

--- Programcion l/herencia/src/Runner.java
+++ Programcion l/herencia/src/Runner.java
@@ -13,10 +13,22 @@ public class Runner {
         showFigure( rectangle, myWindow );
 
         myWindow.getGraphics().drawPolygon(new int[]{60,120,300,280},new int[]{120,180,80,420},4);
+
+        Figure Triangle = new Triangle(new Point(300,250),Color.GREEN, new Point(150,150),new Point(100,100));
+        showFigure(Triangle, myWindow);
+
+
     }
 
     static private void showFigure(Figure figure, JFrame window){
         System.out.printf("El Area es %f\n",figure.calcArea());
         figure.drawFigure(window.getGraphics());
+        if (figure instanceof Circle){
+            System.out.println("cicle");
+
+        }else if (figure instanceof Rectangle){
+            System.out.println("Rectangulo");
+        }
+
     }
 }
---
+++ Programcion l/herencia/src/Triangle.java
@@ -0,0 +1,23 @@
+import  java.awt.*;
+public class Triangle extends  Figure {
+    private  Point pointTWO;
+    private Point PointThree;
+
+    public Triangle(Point point, Color color, Point pointTWO, Point pointThree) {
+        super(point, color);
+        this.pointTWO = pointTWO;
+        PointThree = pointThree;
+    }
+
+    @Override
+    public void drawFigure(Graphics graphics) {
+        int[] pointX ={point.x,point.y,point.x};
+        int[] pointY ={point.x,point.y,point.x};
+        graphics.drawPolygon(pointX,pointY,pointX.length);
+    }
+
+    @Override
+    public double calcArea() {
+        return 0;
+    }
+}
