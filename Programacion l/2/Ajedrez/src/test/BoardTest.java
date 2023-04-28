3b5ca62fa3883a6735e641a7ad3821b5b0c3eebe
SebastianVega4 <sebastian.vegar2015@gmail.com>
2023-04-28 13:42:47 -0500

    ajedrez funcinal

--- Programacion l/2/Ajedrez/src/test/BoardTest.java
+++ Programacion l/2/Ajedrez/src/test/BoardTest.java
@@ -2,8 +2,6 @@ package test;
 
 import co.edu.uptc.model.Board;
 import co.edu.uptc.model.Piece;
-import co.edu.uptc.model.PieceColor;
-import co.edu.uptc.model.PieceType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,65 +9,42 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
-    /*
-    private Board board;
 
-    @BeforeEach
-    void setUp() {
-        board = new Board();
-    }
-
-    @AfterEach
-    void tearDown() {
-        board = null;
-    }
-
-    @Test
-    void getPieceAt() {
-        // Test getting a piece from a valid position
-        Piece piece = board.getPieceAt(0, 0);
-        assertNotNull(piece);
-
-        // Test getting a piece from an invalid position
-        piece = board.getPieceAt(-1, -1);
-        assertNull(piece);
-    }
-
-    @Test
-    void setPieceAt() {
-        // Test setting a piece at a valid position
-        Piece piece = new Piece(PieceType.PEON, PieceColor.BLANCA,1,1);
-        board.setPieceAt(2, 2, piece);
-        Piece newPiece = board.getPieceAt(2, 2);
-        assertNotNull(newPiece);
-        assertEquals(newPiece, piece);
-
-        // Test setting a piece at an invalid position
-        board.setPieceAt(-1, -1, piece);
-        newPiece = board.getPieceAt(-1, -1);
-        assertNull(newPiece);
-    }
-
-    @Test
-    void removePieceAt() {
-        // Test removing a piece from a valid position
-        board.removePieceAt(0, 0);
-        Piece piece = board.getPieceAt(0, 0);
-        assertNull(piece);
-
-        // Test removing a piece from an invalid position
-        board.removePieceAt(-1, -1);
-        piece = board.getPieceAt(-1, -1);
-        assertNull(piece);
-    }
+    Board board = new Board();
 
     @Test
     void isValidMovePeon() {
-        // Test valid moves for a white pawn
-        assertTrue(board.isValidMovePeon(new Piece(PieceType.PEON, PieceColor.BLANCA,1,1), 1, 0));
-        assertTrue(board.isValidMovePeon(new Piece(PieceType.PEON, PieceColor.BLANCA,2,0), 2, 0));
+        Piece piece = board.getPieceAt(1, 0);
+        assertEquals(board.isValidMovePeon(piece,3,0),true);
     }
+
     @Test
-    void getGetError() {
-    }*/
+    void isValidMoveCaballo() {
+        Piece piece = board.getPieceAt(0, 1);
+        assertEquals(board.isValidMoveCaballo(piece,2,2),true);
+    }
+
+    @Test
+    void isValidMoveAlfil() {
+        Piece piece = board.getPieceAt(0, 3);
+        assertEquals(board.isValidMoveAlfil(piece,5,2),false);
+    }
+
+    @Test
+    void isValidMoveTorre() {
+        Piece piece = board.getPieceAt(0, 0);
+        assertEquals(board.isValidMoveTorre(piece,6,0),false);
+    }
+
+    @Test
+    void isValidMoveReina() {
+        Piece piece = board.getPieceAt(0, 4);
+        assertEquals(board.isValidMoveReina(piece,7,4),false);
+    }
+
+    @Test
+    void isValidMoveRey() {
+        Piece piece = board.getPieceAt(0, 5);
+        assertEquals(board.isValidMovePeon(piece,1,6),false);
+    }
 }
--- Programacion l/2/Ajedrez/src/test/ControllerTest.java
+++ Programacion l/2/Ajedrez/src/test/ControllerTest.java
@@ -1,91 +1,18 @@
 package test;
 
 import co.edu.uptc.controller.Controller;
-import co.edu.uptc.model.Board;
-import co.edu.uptc.model.PieceColor;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
-/*
     Controller controller = new Controller();
-    private Board board;
-    private PieceColor turno;
-    @BeforeEach
-    void setUp() {
-        Controller controller = new Controller();
-    }
-
-    @AfterEach
-    void tearDown() {
-        controller = null;
-    }
-
-    @Test
-    void gameController() {
-        assertNotNull(controller.showBoard());
-        assertEquals(PieceColor.BLANCA, controller.getTurno());
-    }
-
-    @Test
-    void startGame() {
-        // Para esta prueba, podemos simular la entrada de un movimiento válido
-        // que permita avanzar el juego a la siguiente jugada.
-        assertTrue(controller.movePiece(1, 0, 2, 0));
-        assertEquals(PieceColor.NEGRA, controller.getTurno());
-    }
-
     @Test
     void movePiece() {
-        // Movimiento válido de peón
-        assertTrue(controller.movePiece(1, 0, 2, 0));
-
-
-        // Movimiento inválido de peón
-        assertFalse(controller.movePiece(6, 0, 4, 0));
-
-        // Movimiento válido de caballo
-        assertTrue(controller.movePiece(0, 1, 2, 0));
-
-
-        // Movimiento inválido de caballo
-        assertFalse(controller.movePiece(0, 1, 2, 2));
-
-        // Movimiento válido de alfil
-        assertTrue(controller.movePiece(1, 2, 2, 3));
-
-        // Movimiento inválido de alfil
-        assertFalse(controller.movePiece(1, 2, 3, 3));
-
-        // Movimiento válido de torre
-        assertTrue(controller.movePiece(0, 0, 2, 0));
-
-        // Movimiento inválido de torre
-        assertFalse(controller.movePiece(0, 0, 2, 1));
-
-        // Movimiento válido de reina
-        assertTrue(controller.movePiece(0, 3, 2, 5));
-
-        // Movimiento inválido de reina
-        assertFalse(controller.movePiece(0, 3, 3, 5));
-
-        // Movimiento válido de rey
-        assertTrue(controller.movePiece(0, 4, 1, 3));
-
-
-        // Movimiento inválido de rey
-        assertFalse(controller.movePiece(0, 4, 2, 3));
+        assertEquals(controller.movePiece(1,0,3,0),true);
+        assertEquals(controller.movePiece(7,7,6,0),false);
+        assertEquals(controller.movePiece(6,1,4,1),true);
+        assertEquals(controller.movePiece(0,0,2,0),true);
 
     }
-
-    @Test
-    void showBoard() {
-        assertDoesNotThrow(() -> controller.showBoard());
-    }
-    @Test
-    void showInvalidMoveMessage() {
-    }*/
 }
