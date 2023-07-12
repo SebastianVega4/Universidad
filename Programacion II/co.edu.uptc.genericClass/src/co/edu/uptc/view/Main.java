e0fb4eef89762de9a3f487ed37b05da1c1a32cc0
“SebastianVega4” <sebastian.vegar2015@gmail.com>
2023-07-12 08:58:37 -0500

    class generic two

--- Programacion II/co.edu.uptc.genericClass/src/co/edu/uptc/view/Main.java
+++ Programacion II/co.edu.uptc.genericClass/src/co/edu/uptc/view/Main.java
@@ -8,13 +8,13 @@ public class Main {
     public static void main(String[] args) {
         List<Object> listaPersonas = new ArrayList<>();
 
-        Estudiante estudiante1 = new Estudiante("Juan", 123);
-        Estudiante estudiante2 = new Estudiante("María", 456);
+        Estudiante estudiante1 = new Estudiante("Juan", 201911913);
+        Estudiante estudiante2 = new Estudiante("María", 202059683);
         listaPersonas.add(estudiante1);
         listaPersonas.add(estudiante2);
 
-        Profesor profesor1 = new Profesor("Pedro", "Matemáticas");
-        Profesor profesor2 = new Profesor("Ana", "Historia");
+        Profesor profesor1 = new Profesor("Pedro", "Sistemas");
+        Profesor profesor2 = new Profesor("Ana", "Contabilidad");
         listaPersonas.add(profesor1);
         listaPersonas.add(profesor2);
 
@@ -24,7 +24,16 @@ public class Main {
         listaPersonas.add(administrativo2);
 
         for (Object persona : listaPersonas) {
-            System.out.println(persona.toString());
+            if (persona instanceof Estudiante) {
+                Estudiante estudiante = (Estudiante) persona;
+                System.out.println("Estudiante: " + estudiante.getName() + ", Código: " + estudiante.getCod());
+            } else if (persona instanceof Profesor) {
+                Profesor profesor = (Profesor) persona;
+                System.out.println("Profesor: " + profesor.getName() + ", Escuela: " + profesor.getEscuela());
+            } else if (persona instanceof Administrativos) {
+                Administrativos administrativo = (Administrativos) persona;
+                System.out.println("Administrativo: " + administrativo.getName() + ", Área: " + administrativo.getArea());
+            }
         }
     }
 }
