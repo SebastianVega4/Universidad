38579e3f27018e2b7056b28da4defbb0527127a4
“SebastianVega4” <sebastian.vegar2015@gmail.com>
2023-07-26 18:57:38 -0500

    Recursividad

--- co.edu.uptc.reculsividad/src/model/Recursividad.java
+++ co.edu.uptc.reculsividad/src/model/Recursividad.java
@@ -1,48 +1,64 @@
 package model;
-    public class Recursividad {
-        private int cont,contSum,conIMatriz,conJmatriz;
-        private String resul,resulSum,resulMatriz ;
-        private int[][] matris={{1,2,3},{4,5,6},{7,8,9}};
 
-        public Recursividad() {
-            this.resul="";
-            this.resulSum="";
-        }
+import java.util.Arrays;
 
-        public int imprimirHastaUnN(int x) {
-            if (x > 0) {
-                imprimirHastaUnN(x - 1);
-            }
-            return x;
-        }
-        public int facR (int n){
-            int resultado;
-            if(n==1)return 1;
-            resultado=facR(n-1)*n;
-            return resultado;
-        }
-        public String tableMul(int n){
-            if(cont != 10){
-                cont++;
-                resul+=cont+"*"+n+"="+(cont*n)+"\n";
-                tableMul(n);
-            }
-             return resul;
-        }
-        public String tableSuma(int n){
-            if(contSum < n){
-                contSum++;
-                resulSum+=contSum+"+"+(n-contSum)+"="+((contSum-n)+n)+"\n";
-                tableSuma(n);
-            }
-            return resulSum;
-        }
-        public String sumaMatriz(){
-            if (conIMatriz==0){
-                if(conJmatriz==0){
-                    
-                }
-            }
-        return resulMatriz;
-        }
+public class Recursividad {
+    private int cont, contSum, conIMatriz, conJmatriz, resulMatriz;
+    private String resul, resulSum, imprimir;
+    private final int[][] matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    public Recursividad() {
+        this.resul = "";
+        this.resulSum = "";
+        this.imprimir = "";
     }
+
+    public String imprimirHastaUnN(int x) {
+        if (x > 0) {
+            imprimir += x + "\n";
+            imprimirHastaUnN(x - 1);
+        }
+        return imprimir;
+    }
+    public int facR(int n) {
+        if (n == 1) return 1;
+        return facR(n - 1) * n;
+    }
+    public String tableMul(int n){
+        if(cont != 10){
+            cont++;
+            resul+=cont+"*"+n+"="+(cont*n)+"\n";
+            tableMul(n);
+        }
+        return resul;
+    }
+
+    public String tableSuma(int n) {
+        if (contSum < n) {
+            contSum++;
+            resulSum += contSum + "+" + (n - contSum) + "=" + ((contSum - n) + n) + "\n";
+            tableSuma(n);
+        }
+        return resulSum;
+    }
+
+    public int sumaMatriz() {
+        if (conIMatriz < matriz.length) {
+            if (conJmatriz < matriz[conIMatriz].length) {
+                resulMatriz += matriz[conIMatriz][conJmatriz];
+                conJmatriz++;
+                sumaMatriz();
+            } else {
+                conIMatriz++;
+                conJmatriz = 0;
+                sumaMatriz();
+            }
+        }
+        return resulMatriz;
+    }
+
+    public String getMatriz() {
+        return Arrays.deepToString(matriz);
+    }
+
+}
--- co.edu.uptc.reculsividad/src/view/Main.java
+++ co.edu.uptc.reculsividad/src/view/Main.java
@@ -5,15 +5,19 @@ import model.Recursividad;
 public class Main {
     public static void main(String[] args) {
         Recursividad re = new Recursividad();
-        /*System.out.println(re.imprimirHastaUnN(5));
-        System.out.println(re.facR(9));
-         */
         System.out.printf("""
+                Imprimir hasta el # dado:
+                %s
+                Numero Factorial 9:
+                %s
                 Tabla de multiplicar:
                 %s
                 Tabla de Suma:
                 %s
-                """,re.tableMul(5), re.tableSuma(8));
+                Suma Matriz:
+                %s
+                la suma de la matriz es: %s
+                """, re.imprimirHastaUnN(5), re.facR(9), re.tableMul(5), re.tableSuma(8), re.getMatriz(), re.sumaMatriz());
     }
 }
 
