bc9dd37b90db25882b0de5ebdd1631f90778e8b0
“SebastianVega4” <sebastian.vegar2015@gmail.com>
2023-07-26 09:52:53 -0500

    genericas

--- co.edu.uptc.reculsividad/src/model/Recursividad.java
+++ co.edu.uptc.reculsividad/src/model/Recursividad.java
@@ -1,5 +1,14 @@
 package model;
     public class Recursividad {
+        private int cont,contSum,conIMatriz,conJmatriz;
+        private String resul,resulSum,resulMatriz ;
+        private int[][] matris={{1,2,3},{4,5,6},{7,8,9}};
+
+        public Recursividad() {
+            this.resul="";
+            this.resulSum="";
+        }
+
         public int imprimirHastaUnN(int x) {
             if (x > 0) {
                 imprimirHastaUnN(x - 1);
@@ -11,5 +20,29 @@ package model;
             if(n==1)return 1;
             resultado=facR(n-1)*n;
             return resultado;
+        }
+        public String tableMul(int n){
+            if(cont != 10){
+                cont++;
+                resul+=cont+"*"+n+"="+(cont*n)+"\n";
+                tableMul(n);
+            }
+             return resul;
+        }
+        public String tableSuma(int n){
+            if(contSum < n){
+                contSum++;
+                resulSum+=contSum+"+"+(n-contSum)+"="+((contSum-n)+n)+"\n";
+                tableSuma(n);
+            }
+            return resulSum;
+        }
+        public String sumaMatriz(){
+            if (conIMatriz==0){
+                if(conJmatriz==0){
+                    
+                }
+            }
+        return resulMatriz;
         }
     }
--- co.edu.uptc.reculsividad/src/view/Main.java
+++ co.edu.uptc.reculsividad/src/view/Main.java
@@ -5,8 +5,15 @@ import model.Recursividad;
 public class Main {
     public static void main(String[] args) {
         Recursividad re = new Recursividad();
-        System.out.println(re.imprimirHastaUnN(5));
+        /*System.out.println(re.imprimirHastaUnN(5));
         System.out.println(re.facR(9));
+         */
+        System.out.printf("""
+                Tabla de multiplicar:
+                %s
+                Tabla de Suma:
+                %s
+                """,re.tableMul(5), re.tableSuma(8));
     }
 }
 
